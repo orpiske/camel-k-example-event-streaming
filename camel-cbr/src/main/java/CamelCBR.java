0502394acab6095ffc6d110b67b4c5312263a0f8
Otavio Rodolfo Piske <opiske@redhat.com>
2020-02-17 18:50:06 +0100

    Added a content-based router that can route the data to different queues

---
+++ camel-cbr/src/main/java/CamelCBR.java
@@ -0,0 +1,32 @@
+import org.apache.camel.PropertyInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.sjms2.Sjms2Component;
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CamelCBR extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(CamelCBR.class);
+
+    @PropertyInject("messaging.broker.url")
+    String brokerUrl;
+
+    public void configure() throws Exception {
+        Sjms2Component sjms2Component = new Sjms2Component();
+        sjms2Component.setConnectionFactory(new JmsConnectionFactory(brokerUrl));
+        getContext().addComponent("sjms2", sjms2Component);
+
+        from("kafka:pm-data?brokers={{kafka.bootstrap.address}}")
+                .log("log:info received => ${body}")
+                .streamCaching()
+                .choice()
+                .when(body().contains("parameter=pm10"))
+                    .log("log:info PM10 Data = ${body}")
+                    .to("sjms2://queue:data.pm10.queue")
+                .when(body().contains("parameter=pm25"))
+                    .log("log:info PM25 Data = ${body}")
+                    .to("sjms2://queue:data.pm25.queue")
+                .otherwise()
+                    .log("log:info We don't need this data on this example = ${body}");
+    }
+}
