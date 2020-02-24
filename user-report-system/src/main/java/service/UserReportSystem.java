e04892be5a23e45d6d2e446addbce503361f790c
Otavio Rodolfo Piske <opiske@redhat.com>
2020-02-24 16:08:03 +0100

    WIP: added new gatekeeper code

--- user-report-system/src/main/java/service/UserReportSystem.java
+++ user-report-system/src/main/java/service/UserReportSystem.java
@@ -5,20 +5,28 @@ import org.apache.camel.builder.RouteBuilder;
 public class UserReportSystem extends RouteBuilder {
 
     public void configure() throws Exception {
-//        from("platform-http:/report/new?httpMethodRestrict=POST")
-//                .to("log:info ${body}");
-
         restConfiguration()
+                .component("netty-http")
                 .host("0.0.0.0")
                 .port("8080");
 
-        rest("/report")
-                .post("/new").to("direct:report-new");
+        rest("/")
+                .get("/report/list").to("direct:get-root")
+                .put("/report/new").to("direct:report-new");
+
+        from("direct:get-root")
+                .transform().constant("Nothing to see here, move along ;) ");
 
         from("direct:report-new")
                 .streamCaching()
-                .wireTap("log:info ${body}").end()
-                .transform().constant("Hello from knative:)");
+                .wireTap("direct:log").end()
+                .to("knative:channel/authentication");
+// For debugging
+//                .transform().constant("Hello from knative :)");
+
+        from("direct:log")
+                .convertBodyTo(String.class)
+                .to("log:info");
 
     }
 }
