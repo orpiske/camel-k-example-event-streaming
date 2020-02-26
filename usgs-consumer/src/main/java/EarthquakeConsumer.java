987e68c38f4c70d2b150242587752c432f6fe5a0
Otavio Rodolfo Piske <opiske@redhat.com>
2020-02-26 12:53:31 +0100

    Fixed to use the correct parameters for publishing earthquake events

--- usgs-consumer/src/main/java/EarthquakeConsumer.java
+++ usgs-consumer/src/main/java/EarthquakeConsumer.java
@@ -9,7 +9,7 @@ public class EarthquakeConsumer extends RouteBuilder {
          route, setting up the HTTP method to GET and hitting the OpenAQ measurement API.
          */
         from("timer:refresh?period=10m&fixedRate=true")
-                .log("OpenAQ route running")
+                .log("USGS Earthquake route running")
                 .setHeader(Exchange.HTTP_METHOD).constant("GET")
                 .to("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson")
                 .streamCaching()
@@ -19,13 +19,13 @@ public class EarthquakeConsumer extends RouteBuilder {
                 measurement separately. To do, we use a splitter to split the results array and send to Kafka
                 only the results data and nothing more.
                 */
-                .split().jsonpath("$.results[*]")
+                .split().jsonpath("$.features[*]")
                 /*
                  Then setup a wireTap route to log the data before sending it to our Kafka instance.
                  */
 
                 .wireTap("direct:tap")
-                .to("kafka:pm-data?brokers={{kafka.bootstrap.address}}");
+                .to("kafka:earthquake-data?brokers={{kafka.bootstrap.address}}");
 
         from("direct:tap")
                 .to("log:info");
