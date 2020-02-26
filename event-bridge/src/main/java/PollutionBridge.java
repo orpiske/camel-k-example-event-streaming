38ec75a893a557cd741a65d9947db77b3d84d49e
Otavio Rodolfo Piske <opiske@redhat.com>
2020-02-26 16:08:30 +0100

    Refactored the old CBR to serve as a bridge for the front-end

--- event-bridge/src/main/java/PollutionBridge.java
+++ event-bridge/src/main/java/PollutionBridge.java
@@ -3,7 +3,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.sjms2.Sjms2Component;
 import org.apache.qpid.jms.JmsConnectionFactory;
 
-public class CamelCBR extends RouteBuilder {
+public class PollutionBridge extends RouteBuilder {
     @PropertyInject("messaging.broker.url")
     String brokerUrl;
 
@@ -13,16 +13,12 @@ public class CamelCBR extends RouteBuilder {
         getContext().addComponent("sjms2", sjms2Component);
 
         from("kafka:pm-data?brokers={{kafka.bootstrap.address}}")
+                .log("log:info Pollution Data = ${body}");
+
+        from("kafka:earthquake-data?brokers={{kafka.bootstrap.address}}")
                 .log("log:info received => ${body}")
                 .streamCaching()
-                .choice()
-                .when(body().contains("parameter=pm10"))
-                    .log("log:info PM10 Data = ${body}")
-                    .to("sjms2://queue:data.pm10.queue")
-                .when(body().contains("parameter=pm25"))
-                    .log("log:info PM25 Data = ${body}")
-                    .to("sjms2://queue:data.pm25.queue")
-                .otherwise()
-                    .log("log:info We don't need this data on this example = ${body}");
+                .log("log:info Earthquake Data = ${body}");
+
     }
 }
